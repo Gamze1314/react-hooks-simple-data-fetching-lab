4ada6b0be98632065161f796cd33991e52a1e5e4
gamze <gamzekandara@gmail.com>
2024-02-11 16:04:38 -0500

    Adds App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useEffect , useState } from 'react'
+
+export default function App() {
+    // use useEffect hook to fetch random dog images
+
+    const [ image, setImage ] = useState("")
+    const [isLoading, setIsLoading] = useState(true); // to check whether img has finished loading
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(data => {
+        setImage(data.message); // update state with random img we fetched from API
+        setIsLoading(!true); // update state
+      });
+
+  }, []);
+
+
+  return (
+    <div>
+        {isLoading ? <p>Loading...</p> : <img src={image} alt="A Random Dog" />}
+    </div>
+  );
+}
+
